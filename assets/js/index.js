719341479bd81064c9ce02a07e61c6fc0f5a5de1
SmartField-IoT <74399519+SmartField-IoT@users.noreply.github.com>
2020-11-20 10:21:25 +0700

    Create index.js

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyC2xHRAm7d8eCU-Emm81lpKmpLjkG9As9g",
+    authDomain: "smartfield-iot.firebaseapp.com",
+    databaseURL: "https://smartfield-iot.firebaseio.com",
+    projectId: "smartfield-iot",
+    storageBucket: "smartfield-iot.appspot.com",
+    messagingSenderId: "946698171545",
+    appId: "1:946698171545:web:49c79169e27f3c615b3408",
+    measurementId: "G-6ZD5J7X6QC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+  //Mengambil data firebase
+    var informasi = firebase.database().ref("User1/");
+    informasi.on("child_added", function (data) {
+        var informasiValue = data.val();
+        var kelembaban_tanah = informasiValue.kelembaban_tanah;
+        var suhu_udara = informasiValue.suhu_udara;
